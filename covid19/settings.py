ce0e129a500221a339d6a4dcada1c0b1c238bce4
vaibhav <it@vertipridesolution.com>
2020-04-21 19:56:27 +0530

    initial commit

--- covid19/settings.py
+++ covid19/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,14 +76,18 @@ WSGI_APPLICATION = 'covid19.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'covid19',
+#         'USER': 'postgres',
+#         'PASSWORD': 'vaibhav',
+#         'HOST': 'localhost'
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'covid19',
-        'USER': 'postgres',
-        'PASSWORD': 'vaibhav',
-        'HOST': 'localhost'
-    }
+    'default': dj_database_url.config()
 }
 
 
