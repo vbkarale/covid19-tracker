74ae052615d22984d5d620c862dbd31d02765a6d
vaibhav <it@vertipridesolution.com>
2020-04-21 18:24:42 +0530

    initial commit

--- assets/global_assets/js/demo_pages/maps/vector/vector_maps_demo.js
+++ assets/global_assets/js/demo_pages/maps/vector/vector_maps_demo.js
@@ -10,7 +10,7 @@
 // Setup module
 // ------------------------------
 
-var VectorMaps = function() {
+var VectorMaps = function () {
 
 
     //
@@ -18,7 +18,7 @@ var VectorMaps = function() {
     //
 
     // Line chart
-    var _vectorMapsExamples = function() {
+    var _vectorMapsExamples = function () {
         if (!$().vectorMap) {
             console.warn('Warning - jvectormap.min.js is not loaded.');
             return;
@@ -55,7 +55,7 @@ var VectorMaps = function() {
                     'fill': '#336BB5',
                     'fill-opacity': 0.8,
                     'stroke': '#fff',
-                    'stroke-width' : 1.5,
+                    'stroke-width': 1.5,
                     'stroke-opacity': 0.9
                 },
                 hover: {
@@ -70,64 +70,64 @@ var VectorMaps = function() {
                 scale: 2
             },
             markers: [
-                {latLng: [41.90, 12.45], name: 'Vatican City'},
-                {latLng: [43.73, 7.41], name: 'Monaco'},
-                {latLng: [40.726, -111.778], name: 'Salt Lake City'},
-                {latLng: [39.092, -94.575], name: 'Kansas City'},
-                {latLng: [25.782, -80.231], name: 'Miami'},
-                {latLng: [8.967, -79.458], name: 'Panama City'},
-                {latLng: [19.400, -99.124], name: 'Mexico City'},
-                {latLng: [40.705, -73.978], name: 'New York'},
-                {latLng: [33.98, -118.132], name: 'Los Angeles'},
-                {latLng: [47.614, -122.335], name: 'Seattle'},
-                {latLng: [44.97, -93.261], name: 'Minneapolis'},
-                {latLng: [39.73, -105.015], name: 'Denver'},
-                {latLng: [41.833, -87.732], name: 'Chicago'},
-                {latLng: [29.741, -95.395], name: 'Houston'},
-                {latLng: [23.05, -82.33], name: 'Havana'},
-                {latLng: [45.41, -75.70], name: 'Ottawa'},
-                {latLng: [53.555, -113.493], name: 'Edmonton'},
-                {latLng: [-0.23, -78.52], name: 'Quito'},
-                {latLng: [18.50, -69.99], name: 'Santo Domingo'},
-                {latLng: [4.61, -74.08], name: 'Bogotá'},
-                {latLng: [14.08, -87.21], name: 'Tegucigalpa'},
-                {latLng: [17.25, -88.77], name: 'Belmopan'},
-                {latLng: [14.64, -90.51], name: 'New Guatemala'},
-                {latLng: [-15.775, -47.797], name: 'Brasilia'},
-                {latLng: [-3.790, -38.518], name: 'Fortaleza'},
-                {latLng: [50.402, 30.532], name: 'Kiev'},
-                {latLng: [53.883, 27.594], name: 'Minsk'},
-                {latLng: [52.232, 21.061], name: 'Warsaw'},
-                {latLng: [52.507, 13.426], name: 'Berlin'},
-                {latLng: [50.059, 14.465], name: 'Prague'},
-                {latLng: [47.481, 19.130], name: 'Budapest'},
-                {latLng: [52.374, 4.898], name: 'Amsterdam'},
-                {latLng: [48.858, 2.347], name: 'Paris'},
-                {latLng: [40.437, -3.679], name: 'Madrid'},
-                {latLng: [39.938, 116.397], name: 'Beijing'},
-                {latLng: [28.646, 77.093], name: 'Delhi'},
-                {latLng: [25.073, 55.229], name: 'Dubai'},
-                {latLng: [35.701, 51.349], name: 'Tehran'},
-                {latLng: [7.11, 171.06], name: 'Marshall Islands'},
-                {latLng: [17.3, -62.73], name: 'Saint Kitts and Nevis'},
-                {latLng: [3.2, 73.22], name: 'Maldives'},
-                {latLng: [35.88, 14.5], name: 'Malta'},
-                {latLng: [12.05, -61.75], name: 'Grenada'},
-                {latLng: [13.16, -61.23], name: 'Saint Vincent and the Grenadines'},
-                {latLng: [13.16, -59.55], name: 'Barbados'},
-                {latLng: [17.11, -61.85], name: 'Antigua and Barbuda'},
-                {latLng: [-4.61, 55.45], name: 'Seychelles'},
-                {latLng: [7.35, 134.46], name: 'Palau'},
-                {latLng: [42.5, 1.51], name: 'Andorra'},
-                {latLng: [14.01, -60.98], name: 'Saint Lucia'},
-                {latLng: [6.91, 158.18], name: 'Federated States of Micronesia'},
-                {latLng: [1.3, 103.8], name: 'Singapore'},
-                {latLng: [1.46, 173.03], name: 'Kiribati'},
-                {latLng: [-21.13, -175.2], name: 'Tonga'},
-                {latLng: [15.3, -61.38], name: 'Dominica'},
-                {latLng: [-20.2, 57.5], name: 'Mauritius'},
-                {latLng: [26.02, 50.55], name: 'Bahrain'},
-                {latLng: [0.33, 6.73], name: 'São Tomé and Príncipe'}
+                { latLng: [41.90, 12.45], name: 'Vatican City' },
+                { latLng: [43.73, 7.41], name: 'Monaco' },
+                { latLng: [40.726, -111.778], name: 'Salt Lake City' },
+                { latLng: [39.092, -94.575], name: 'Kansas City' },
+                { latLng: [25.782, -80.231], name: 'Miami' },
+                { latLng: [8.967, -79.458], name: 'Panama City' },
+                { latLng: [19.400, -99.124], name: 'Mexico City' },
+                { latLng: [40.705, -73.978], name: 'New York' },
+                { latLng: [33.98, -118.132], name: 'Los Angeles' },
+                { latLng: [47.614, -122.335], name: 'Seattle' },
+                { latLng: [44.97, -93.261], name: 'Minneapolis' },
+                { latLng: [39.73, -105.015], name: 'Denver' },
+                { latLng: [41.833, -87.732], name: 'Chicago' },
+                { latLng: [29.741, -95.395], name: 'Houston' },
+                { latLng: [23.05, -82.33], name: 'Havana' },
+                { latLng: [45.41, -75.70], name: 'Ottawa' },
+                { latLng: [53.555, -113.493], name: 'Edmonton' },
+                { latLng: [-0.23, -78.52], name: 'Quito' },
+                { latLng: [18.50, -69.99], name: 'Santo Domingo' },
+                { latLng: [4.61, -74.08], name: 'Bogotá' },
+                { latLng: [14.08, -87.21], name: 'Tegucigalpa' },
+                { latLng: [17.25, -88.77], name: 'Belmopan' },
+                { latLng: [14.64, -90.51], name: 'New Guatemala' },
+                { latLng: [-15.775, -47.797], name: 'Brasilia' },
+                { latLng: [-3.790, -38.518], name: 'Fortaleza' },
+                { latLng: [50.402, 30.532], name: 'Kiev' },
+                { latLng: [53.883, 27.594], name: 'Minsk' },
+                { latLng: [52.232, 21.061], name: 'Warsaw' },
+                { latLng: [52.507, 13.426], name: 'Berlin' },
+                { latLng: [50.059, 14.465], name: 'Prague' },
+                { latLng: [47.481, 19.130], name: 'Budapest' },
+                { latLng: [52.374, 4.898], name: 'Amsterdam' },
+                { latLng: [48.858, 2.347], name: 'Paris' },
+                { latLng: [40.437, -3.679], name: 'Madrid' },
+                { latLng: [39.938, 116.397], name: 'Beijing' },
+                { latLng: [28.646, 77.093], name: 'Delhi' },
+                { latLng: [25.073, 55.229], name: 'Dubai' },
+                { latLng: [35.701, 51.349], name: 'Tehran' },
+                { latLng: [7.11, 171.06], name: 'Marshall Islands' },
+                { latLng: [17.3, -62.73], name: 'Saint Kitts and Nevis' },
+                { latLng: [3.2, 73.22], name: 'Maldives' },
+                { latLng: [35.88, 14.5], name: 'Malta' },
+                { latLng: [12.05, -61.75], name: 'Grenada' },
+                { latLng: [13.16, -61.23], name: 'Saint Vincent and the Grenadines' },
+                { latLng: [13.16, -59.55], name: 'Barbados' },
+                { latLng: [17.11, -61.85], name: 'Antigua and Barbuda' },
+                { latLng: [-4.61, 55.45], name: 'Seychelles' },
+                { latLng: [7.35, 134.46], name: 'Palau' },
+                { latLng: [42.5, 1.51], name: 'Andorra' },
+                { latLng: [14.01, -60.98], name: 'Saint Lucia' },
+                { latLng: [6.91, 158.18], name: 'Federated States of Micronesia' },
+                { latLng: [1.3, 103.8], name: 'Singapore' },
+                { latLng: [1.46, 173.03], name: 'Kiribati' },
+                { latLng: [-21.13, -175.2], name: 'Tonga' },
+                { latLng: [15.3, -61.38], name: 'Dominica' },
+                { latLng: [-20.2, 57.5], name: 'Mauritius' },
+                { latLng: [26.02, 50.55], name: 'Bahrain' },
+                { latLng: [0.33, 6.73], name: 'São Tomé and Príncipe' }
             ]
         });
 
@@ -140,8 +140,8 @@ var VectorMaps = function() {
 
             // Values
             statesValues = jvm.values.apply({}, jvm.values(data.states)),
-            metroPopValues = Array.prototype.concat.apply([], jvm.values(data.metro.population)),
-            metroUnemplValues = Array.prototype.concat.apply([], jvm.values(data.metro.unemployment));
+                metroPopValues = Array.prototype.concat.apply([], jvm.values(data.metro.population)),
+                metroUnemplValues = Array.prototype.concat.apply([], jvm.values(data.metro.unemployment));
 
             // Configuration
             $('.map-unemployment').vectorMap({
@@ -153,7 +153,7 @@ var VectorMaps = function() {
                         r: 6,
                         'fill-opacity': 0.9,
                         'stroke': '#fff',
-                        'stroke-width' : 1.5,
+                        'stroke-width': 1.5,
                         'stroke-opacity': 0.95
                     },
                     hover: {
@@ -188,18 +188,18 @@ var VectorMaps = function() {
                     }]
                 },
 
-                onMarkerLabelShow: function(event, label, index) {
+                onMarkerLabelShow: function (event, label, index) {
                     label.html(
-                        ''+data.metro.names[index]+'<br>'+
-                        'Population: '+data.metro.population[val][index]+'<br>'+
-                        'Unemployment rate: '+data.metro.unemployment[val][index]+'%'
+                        '' + data.metro.names[index] + '<br>' +
+                        'Population: ' + data.metro.population[val][index] + '<br>' +
+                        'Unemployment rate: ' + data.metro.unemployment[val][index] + '%'
                     );
                 },
 
-                onRegionLabelShow: function(event, label, code) {
+                onRegionLabelShow: function (event, label, code) {
                     label.html(
-                        ''+label.html()+'<br>'+
-                        'Unemployment rate: '+data.states[val][code]+'%'
+                        '' + label.html() + '<br>' +
+                        'Unemployment rate: ' + data.states[val][code] + '%'
                     );
                 }
             });
@@ -220,8 +220,11 @@ var VectorMaps = function() {
                     normalizeFunction: 'polynomial'
                 }]
             },
-            onRegionLabelShow: function(e, el, code){
-                el.html(el.html()+'<br>'+'GDP - '+gdpData[code]);
+            onRegionLabelShow: function (e, el, code) {
+                el.html(el.html() + '<br>' + 'confirmed - ' + gdpData[code][0]
+                    + '<br>' + 'Recovered - ' + gdpData[code][1]
+                    + '<br>' + 'Deaths - ' + gdpData[code][2]
+                    + '<br>' + 'Cases 1m per people - ' + gdpData[code][3]);
             }
         });
 
@@ -232,29 +235,29 @@ var VectorMaps = function() {
 
         // Set data
         markers = [
-            {latLng: [52.50, 13.39], name: 'Berlin'},
-            {latLng: [53.56, 10.00], name: 'Hamburg'},
-            {latLng: [48.13, 11.56], name: 'Munich'},
-            {latLng: [50.95, 6.96], name: 'Cologne'},
-            {latLng: [50.11, 8.68], name: 'Frankfurt am Main'},
-            {latLng: [48.77, 9.17], name: 'Stuttgart'},
-            {latLng: [51.23, 6.78], name: 'Düsseldorf'},
-            {latLng: [51.51, 7.46], name: 'Dortmund'},
-            {latLng: [51.45, 7.01], name: 'Essen'},
-            {latLng: [53.07, 8.80], name: 'Bremen'}
+            { latLng: [52.50, 13.39], name: 'Berlin' },
+            { latLng: [53.56, 10.00], name: 'Hamburg' },
+            { latLng: [48.13, 11.56], name: 'Munich' },
+            { latLng: [50.95, 6.96], name: 'Cologne' },
+            { latLng: [50.11, 8.68], name: 'Frankfurt am Main' },
+            { latLng: [48.77, 9.17], name: 'Stuttgart' },
+            { latLng: [51.23, 6.78], name: 'Düsseldorf' },
+            { latLng: [51.51, 7.46], name: 'Dortmund' },
+            { latLng: [51.45, 7.01], name: 'Essen' },
+            { latLng: [53.07, 8.80], name: 'Bremen' }
         ],
-        cityAreaData = [
-            887.70,
-            755.16,
-            310.69,
-            405.17,
-            248.31,
-            207.35,
-            217.22,
-            280.71,
-            210.32,
-            325.42
-        ]
+            cityAreaData = [
+                887.70,
+                755.16,
+                310.69,
+                405.17,
+                248.31,
+                207.35,
+                217.22,
+                280.71,
+                210.32,
+                325.42
+            ]
 
         // Configuration
         var map = new jvm.WorldMap({
@@ -268,7 +271,7 @@ var VectorMaps = function() {
                 initial: {
                     'fill': '#E77644',
                     'stroke': '#fff',
-                    'stroke-width' : 1.5,
+                    'stroke-width': 1.5,
                     'stroke-opacity': 0.9
                 },
                 hover: {
@@ -297,7 +300,7 @@ var VectorMaps = function() {
                     values: cityAreaData
                 }]
             },
-            onRegionSelected: function(){
+            onRegionSelected: function () {
                 if (window.localStorage) {
                     window.localStorage.setItem(
                         'jvectormap-selected-regions',
@@ -305,7 +308,7 @@ var VectorMaps = function() {
                     );
                 }
             },
-            onMarkerSelected: function(){
+            onMarkerSelected: function () {
                 if (window.localStorage) {
                     window.localStorage.setItem(
                         'jvectormap-selected-markers',
@@ -316,8 +319,8 @@ var VectorMaps = function() {
         });
 
         // Set regions
-        map.setSelectedRegions( JSON.parse( window.localStorage.getItem('jvectormap-selected-regions') || '[]' ) );
-        map.setSelectedMarkers( JSON.parse( window.localStorage.getItem('jvectormap-selected-markers') || '[]' ) );
+        map.setSelectedRegions(JSON.parse(window.localStorage.getItem('jvectormap-selected-regions') || '[]'));
+        map.setSelectedMarkers(JSON.parse(window.localStorage.getItem('jvectormap-selected-markers') || '[]'));
     };
 
 
@@ -326,7 +329,7 @@ var VectorMaps = function() {
     //
 
     return {
-        init: function() {
+        init: function () {
             _vectorMapsExamples();
         }
     }
@@ -336,6 +339,6 @@ var VectorMaps = function() {
 // Initialize module
 // ------------------------------
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     VectorMaps.init();
 });
